37b8ee556a3c1b7a45074f73b3701b9a9cd3ec9f
tv <lytvynenkotv@gmail.com>
2025-01-18 15:43:19 +0200

    updated version

--- profiling.py
+++
@@ -1,9 +0,0 @@
-from numeral_converter import numeral2int
-
-import cProfile
-
-import logging
-logging.basicConfig(level=logging.ERROR)
-
-numeral2int('сто', 'uk')
-cProfile.run("numeral2int('сто', 'uk')")
